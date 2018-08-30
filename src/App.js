5ecbaeeaa29a1c38db469ca79c250ac8b72a5850
David Chu <davidchuyaya@gmail.com>
2018-08-30 00:25:46 -0400

    Start using material library, created app bar

--- src/App.js
+++ src/App.js
@@ -1,21 +1,54 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
+import AppBar from "@material-ui/core/AppBar/AppBar";
+import Toolbar from "@material-ui/core/Toolbar/Toolbar";
+import Typography from "@material-ui/core/Typography/Typography";
+import { withStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button/Button";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+class App extends Component
+{
+	render()
+	{
+		const {classes} = this.props;
+		return (
+			<React.Fragment>
+				<CssBaseline />
+				<div className={classes.root}>
+					<AppBar color={"default"}>
+						<Toolbar>
+							<Typography variant={"title"} color={"inherit"} className={classes.title}>
+								Events
+							</Typography>
+							<Typography variant={"title"} color={"inherit"}>
+								Are you an organization?
+							</Typography>
+							<Button color={"primary"} className={classes.button}>
+								Log in
+							</Button>
+							<Button variant={"outlined"} color={"primary"} className={classes.button}>
+								Sign up
+							</Button>
+						</Toolbar>
+					</AppBar>
+				</div>
+			</React.Fragment>
+		);
+	}
 }
 
-export default App;
+const styles = {
+	root: {
+		flexGrow: 1
+	},
+	title: {
+		flexGrow: 1
+	},
+	button: {
+
+	}
+};
+
+export default withStyles(styles)(App);
